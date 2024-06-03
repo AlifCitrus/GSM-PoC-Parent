272a584c788e216043b42a6627f26c3084d2f00f
AlifCitrus <alif.shamsudheen@citrusinformatics.com>
2024-06-03 14:26:04 +0530

    Submodules are added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ChildSampleComponent } from './child-sample/child-sample.component';
 
 const routes: Routes = [
-  // { path: 'app1',  loadChildren: () => import('../../projects/app1/src/app/app.module').then(m => m.AppModule)},
-  // { path: 'app2',  loadChildren: () => import('../../projects/app2/src/app/app.module').then(m => m.AppModule)},
-
-  { path:  'child-app-1', component:ChildSampleComponent },
-
+  { path: 'child-app-1',  loadChildren: () => import('../../projects/gsm-po-c-child-1/src/app/app.module').then(m => m.AppModule)},
+  { path: 'child-app-2',  loadChildren: () => import('../../projects/gsm-po-c-child-2/src/app/app.module').then(m => m.AppModule)},
   { path: '**', redirectTo: 'child-app-1'}
 ];
 
